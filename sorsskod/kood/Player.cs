7a1fc090b4dbb8998d216d422c920d74516c904d
P3-J <jako.keskkyla@gmail.com>
2026-01-29 18:38:11 +0200

    4.6 ja plugin fix

--- sorsskod/kood/Player.cs
+++ sorsskod/kood/Player.cs
@@ -2,56 +2,67 @@ using Godot;
 
 public partial class Player : CharacterBody3D
 {
-    [Export] public float Speed = 6f;
-    [Export] public float JumpVelocity = 4.5f;
-    [Export] public float MouseSensitivity = 0.002f;
-    [Export] private Node3D _head;
-    [Export] private Camera3D _camera;
+	[Export] public float Speed = 6f;
+	[Export] public float JumpVelocity = 4.5f;
+	[Export] public float MouseSensitivity = 0.002f;
+	[Export] private Node3D _head;
+	[Export] private Camera3D _camera;
 
-    public override void _Ready()
-    {
+	public override void _Ready()
+	{
 
-        Input.MouseMode = Input.MouseModeEnum.Captured;
-    }
+		Input.MouseMode = Input.MouseModeEnum.Captured;  git config --global user.email "jako.keskkyla@gmail.com"
+  															git config --global user.name "Your Name"
 
-    public override void _UnhandledInput(InputEvent e)
-    {
-        if (e is InputEventMouseMotion mouseMotion)
-        {
-            RotateY(-mouseMotion.Relative.X * MouseSensitivity);
-            _head.RotateX(-mouseMotion.Relative.Y * MouseSensitivity);
+	}
 
-            Vector3 rot = _head.Rotation;
-            rot.X = Mathf.Clamp(rot.X, Mathf.DegToRad(-89), Mathf.DegToRad(89));
-            _head.Rotation = rot;
-        }
-    }
+	public override void _UnhandledInput(InputEvent e)
+	{
+		if (e is InputEventMouseMotion mouseMotion)
+		{
+			RotateY(-mouseMotion.Relative.X * MouseSensitivity);
+			_head.RotateX(-mouseMotion.Relative.Y * MouseSensitivity);
 
-    public override void _PhysicsProcess(double delta)
-    {
-        Vector3 velocity = Velocity;
+			Vector3 rot = _head.Rotation;
+			rot.X = Mathf.Clamp(rot.X, Mathf.DegToRad(-89), Mathf.DegToRad(89));
+			_head.Rotation = rot;
+		}
 
-        if (!IsOnFloor())
-            velocity.Y -= ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle() * (float)delta;
-    
-        if (Input.IsActionJustPressed("jump") && IsOnFloor())
-            velocity.Y = JumpVelocity;
+		if (e is InputEventKey)
+		{
 
-        Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
-        Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+			if (e.IsActionPressed("escape"))
+			{
+				Input.MouseMode = Input.MouseMode == Input.MouseModeEnum.Captured ? Input.MouseModeEnum.Visible : Input.MouseModeEnum.Captured;
+			}
+		}
+	}
 
-        if (direction != Vector3.Zero)
-        {
-            velocity.X = direction.X * Speed;
-            velocity.Z = direction.Z * Speed;
-        }
-        else
-        {
-            velocity.X = Mathf.MoveToward(velocity.X, 0, Speed);
-            velocity.Z = Mathf.MoveToward(velocity.Z, 0, Speed);
-        }
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
 
-        Velocity = velocity;
-        MoveAndSlide();
-    }
+		if (!IsOnFloor())
+			velocity.Y -= ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle() * (float)delta;
+
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		Vector2 inputDir = Input.GetVector("move_left", "move_right", "move_forward", "move_back");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(velocity.Z, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
 }
