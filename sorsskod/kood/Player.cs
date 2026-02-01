b9508b60f77437701c7aa8167ecd9bbc4678df75
Jako Keskküla <jako.keskkyla@gmail.com>
2026-02-01 16:28:02 +0200

    fixes bruuuh

--- sorsskod/kood/Player.cs
+++ sorsskod/kood/Player.cs
@@ -11,6 +11,7 @@ public partial class Player : CharacterBody3D
 	[Export] Marker3D handMarker;
 	[Export] CashRegister reg;
 	[Export] Camera3D cashCam;
+	[Export] AudioStreamPlayer3D craftingAudio;
 	Label toiletLabel;
 	Label infoTag;
 
