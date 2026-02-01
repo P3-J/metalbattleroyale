e4dee1696a7f5bbbe543aff96b01470ff52bf628
Jako Keskküla <jako.keskkyla@gmail.com>
2026-02-01 13:30:03 +0200

    masks

--- sorsskod/kood/ActObj.cs
+++ sorsskod/kood/ActObj.cs
@@ -6,8 +6,12 @@ public partial class ActObj : RigidBody3D
 	
 	[Export] MeshInstance3D f1;
 	[Export] MeshInstance3D f2;
+	[Export] MeshInstance3D f3;
+	[Export] MeshInstance3D f4;
+	[Export] MeshInstance3D f5;
 
-	string[] masks = ["Protective Mask", "Ninja Mask"];
+
+	string[] masks = ["Protective Mask", "Ninja Mask", "Festival Mask", "Human Skin Mask", "Blinding Mask"];
 	string thisMask = "Protective Mask";
 
 
@@ -19,17 +23,29 @@ public partial class ActObj : RigidBody3D
 
 	public void MakeMask(string masktype)
 	{
-		
+
 
 		switch (masktype)
 		{
-			
+
 			case "Protective Mask":
 				f1.Visible = true;
 				thisMask = "Protective Mask";
 				break;
 			case "Ninja Mask":
 				f2.Visible = true;
+				thisMask = "Ninja Mask";
+				break;
+			case "Festival Mask":
+				f3.Visible = true;
+				thisMask = "Ninja Mask";
+				break;
+			case "Human Skin Mask":
+				f4.Visible = true;
+				thisMask = "Ninja Mask";
+				break;
+			case "Blinding Mask":
+				f5.Visible = true;
 				thisMask = "Ninja Mask";
 				break;
 
