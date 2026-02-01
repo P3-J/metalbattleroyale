968c7ec3325d15da6d7033c73f303d120b9f81ef
Jako Keskküla <44132378+P3-J@users.noreply.github.com>
2026-02-01 14:24:11 +0200

    Polish brothers (#26)
    * masks
    
    * gamershit
    
    * map changes

--- sorsskod/kood/ActObj.cs
+++ sorsskod/kood/ActObj.cs
@@ -6,9 +6,13 @@ public partial class ActObj : RigidBody3D
 	
 	[Export] MeshInstance3D f1;
 	[Export] MeshInstance3D f2;
+	[Export] MeshInstance3D f3;
+	[Export] MeshInstance3D f4;
+	[Export] MeshInstance3D f5;
 
-	string[] masks = ["Protective Mask", "Ninja Mask"];
-	public string thisMask = "Protective Mask";
+
+	string[] masks = ["Protective Mask", "Ninja Mask", "Festival Mask", "Human Skin Mask", "Blinding Mask"];
+	string thisMask = "Protective Mask";
 
 
 	public override void _Ready()
@@ -19,11 +23,11 @@ public partial class ActObj : RigidBody3D
 
 	public void MakeMask(string masktype)
 	{
-		
+
 
 		switch (masktype)
 		{
-			
+
 			case "Protective Mask":
 				f1.Visible = true;
 				thisMask = "Protective Mask";
@@ -31,6 +35,18 @@ public partial class ActObj : RigidBody3D
 			case "Ninja Mask":
 				f2.Visible = true;
 				thisMask = "Ninja Mask";
+				break;
+			case "Festival Mask":
+				f3.Visible = true;
+				thisMask = "Festival Mask";
+				break;
+			case "Human Skin Mask":
+				f4.Visible = true;
+				thisMask = "Human Skin Mask";
+				break;
+			case "Blinding Mask":
+				f5.Visible = true;
+				thisMask = "Blinding Mask";
 				break;
 
 		}
