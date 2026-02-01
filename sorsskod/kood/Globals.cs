0c7b6a522190cdca4699caf38b2b7b10f9ad3a4a
Stremper <70448635+Stremper@users.noreply.github.com>
2026-02-01 13:02:18 +0200

    Upgradable passive income bench + debug balance view on UI

--- sorsskod/kood/Globals.cs
+++ sorsskod/kood/Globals.cs
@@ -7,5 +7,7 @@ public partial class Globals : Node
     [Signal] public delegate void SendItemsToRegisterEventHandler(string[] itemNames);
     [Signal] public delegate void OrderInEventHandler();
     [Signal] public delegate void OrderDoneEventHandler(bool Done, int orderValue = 0);
+    [Signal] public delegate void PassiveBenchInteractEventHandler(int money);
+    [Signal] public delegate void PassiveBenchResponseEventHandler(int moneyChange, int newNeededMoney);
 
 }
